d297584602542bcea9e27667a27f00da92997b3f
Mariia Hnied <mariiahnied@berkeley.edu>
2021-06-25 13:25:34 -0700

    updated hackervoice function to check for a correct password

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,8 +2,17 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     var password = (req.query.password);
 
+    let your_response;
+
+    if(password == "letmein"){
+        your_response = "Access granted."
+    }
+    else {
+        your_response = "Access denied."
+    }
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: your_response
     };
 }
