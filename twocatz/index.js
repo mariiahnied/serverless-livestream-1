45719a1920704262d072a078a14c52e6a95d63b8
Mariia Hnied <mariiahnied@berkeley.edu>
2021-06-25 22:56:52 -0700

    created a function that returns two pictures of cats, encoded in base64  and two random names in JSON format in the body

--- twocatz/index.js
+++ twocatz/index.js
@@ -1,18 +1,39 @@
 const fetch = require('node-fetch')
 
+
+function generate_name(){
+    var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+    var random_value = Math.floor(names.length * Math.random());
+    var resultname = names[random_value];
+    return resultname
+
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+    let endpoint =  "https://cataas.com/cat/cute/says/Bitcamp";
+
+    let name1 = generate_name();
+    let name2 = generate_name();
     
-    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless", {
+    let resp1 = await fetch(endpoint, {
         method: 'GET'
     });
-    
-    let data = await resp.arrayBuffer()
-  
-    let base64data = Buffer.from(data).toString('base64')
+    let data1 = await resp1.arrayBuffer()
+    let base64data1 = Buffer.from(data1).toString('base64')
 
+    let resp2 = await fetch(endpoint, {
+        method: 'GET'
+    });
+    let data2 = await resp2.arrayBuffer()
+    let base64data2 = Buffer.from(data2).toString('base64')
+    
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: { base64data }
+        body: {
+            cat1: base64data1,
+            cat2: base64data2,
+            names: [name1, name2]
+        }
     };
 }
