c8b854ab93a95974ef06f6690a54825cb717954c
Mariia Hnied <mariiahnied@berkeley.edu>
2021-07-04 08:05:04 -0700

    updated emotionalgifs function to return a gif corresponding to a dominant emotion on a picture

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -18,13 +18,15 @@ module.exports = async function (context, req) {
     var result = await analyzeImage(imageData);
 
     let emotions = result[0].faceAttributes.emotion;
-    let objects = Object.values(emotions);
-    // FILL IT IN
-    // What your array could look like: [0.01, 0.34, .....]
+    let objects = Object.values(emotions); // What your array could look like: [0.01, 0.34, .....]
     const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+
+    var gif = await findGifs(main_emotion);
+
     context.res = {
-        body: main_emotion
-};
+        // status: 200, /* Defaults to 200 */
+        body:gif.data.url
+    };
 
     console.log(result)
     context.done(); 
@@ -36,7 +38,6 @@ async function analyzeImage(img){
     //change values while testing...
     const subscriptionKey = process.env.FACEAPI_KEY1; 
     const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
-    
     //...to hard values
 
     let params = new URLSearchParams({
@@ -44,7 +45,7 @@ async function analyzeImage(img){
         'returnFaceAttributes': 'emotion'
     })
     
-    let resp = await fetch(uriBase + '?' + params.toString(), {
+    let resp = await fetch (uriBase + '?' + params.toString(), {
         method: 'POST',  //WHAT TYPE OF REQUEST?
         body: img,  //WHAT ARE WE SENDING TO THE API?
         headers: {
@@ -54,5 +55,14 @@ async function analyzeImage(img){
     })
     let data = await resp.json();
     
-    return data; 
+    return data;
+} 
+
+async function findGifs(emotion){
+    const giphykey = process.env.GIF_KEY
+    
+    let gifresponse = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=" + giphykey + "&s=" + emotion);
+    const gifresp = await gifresponse.json();
+    return gifresp;
 }
+
