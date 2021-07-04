49299c6d88fca71ff1ecb53ab6ad054e94ef0a67
Mariia Hnied <mariiahnied@berkeley.edu>
2021-07-04 06:10:08 -0700

    updated emotionalgifs function so it returns the dominant emotion

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -15,19 +15,19 @@ module.exports = async function (context, req) {
     //var convertedResult = Buffer.from(parts[0].data).toString('base64');
     var imageData = parts[0].data;
 
-    
-    //module.exports function
-    //analyze the image
     var result = await analyzeImage(imageData);
+
+    let emotions = result[0].faceAttributes.emotion;
+    let objects = Object.values(emotions);
+    // FILL IT IN
+    // What your array could look like: [0.01, 0.34, .....]
+    const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
     context.res = {
-        body: {
-            result
-        }
-    };
+        body: main_emotion
+};
+
     console.log(result)
     context.done(); 
-
-
 }
 
 
@@ -37,10 +37,7 @@ async function analyzeImage(img){
     const subscriptionKey = process.env.FACEAPI_KEY1; 
     const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
     
-    //...to this
-    //const subscriptionKey = "cd78cabb10ff4efdb04cc2da471d6444";
-    //const uriBase = "https://gettingemotional2.cognitiveservices.azure.com/face/v1.0/detect";
-
+    //...to hard values
 
     let params = new URLSearchParams({
         'returnFaceId': 'true',
