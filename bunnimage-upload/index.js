4c0941d314248447610e73741e7572a458e86f69
Mariia Hnied <mariiahnied@berkeley.edu>
2021-07-06 22:53:23 -0700

    function bunnimage saves an image into blob container

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,13 +1,39 @@
-npmodule.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var multipart = require("parse-multipart")
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const { BlobServiceClient } = require("@azure/storage-blob");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+module.exports = async function (context, req) {
+
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    var body = req.body;
+    var parsedBody = multipart.Parse(body, boundary);
+
+    var filetype = parsedBody[0].type;
+    if (filetype == "image/png") {
+        ext = "png";
+    } else if (filetype == "image/jpeg") {
+        ext = "jpg";
+    } else {
+        username = "invalidimage"
+        ext = "";
+    }
+
+    var responseMessage = await uploadFile(parsedBody, ext);
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+}
+
+async function uploadFile(parsedBody, ext){ //"ext" for a  file extension
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const containerName = "images";
+    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
+
+    const blobName = 'test.' + ext;    // Create the container
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
+
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+
+    return "Your blob is saved"
 }
