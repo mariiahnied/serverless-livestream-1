204e03976e20af96bd1eb9b4fe68cfdf1acc4d29
test <test@Mariias-MacBook-Pro.local>
2021-07-10 21:09:25 -0700

    changed in bunnimage-dwnload url grom jpg back to jpeg

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
     let blobStorage = "mh1"
     var download = ""
     var downloadpng = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".png";
-    var downloadjpg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpg";
+    var downloadjpg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpeg";
 
     let pngresp = await fetch(downloadpng, {
         method: 'GET',
