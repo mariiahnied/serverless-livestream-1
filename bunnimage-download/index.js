031e0026863bcc41fdb24c96afe449acc23bd54b
test <test@Mariias-MacBook-Pro.local>
2021-07-10 20:55:03 -0700

    updated bunnimage-upload and bunnimage-download, so both jpeg and jpg are being supported

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -1,12 +1,11 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     var username = req.headers['username']; //username is the name of the blob fikle you wannt to download
     let blobStorage = "mh1"
-    var download = "hi"
+    var download = ""
     var downloadpng = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".png";
-    var downloadjpg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpg";
-    var downloadjpeg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpeg";
+    var downloadjpg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpeg";
 
     let pngresp = await fetch(downloadpng, {
         method: 'GET',
@@ -18,17 +17,12 @@ module.exports = async function (context, req) {
     })
     let jpgdata = await jpgresp;
 
-    let jpegresp = await fetch(downloadjpeg, {
-        method: 'GET',
-        })
-    let jpegdata = await jpegresp;
 
-    if (pngdata.statusText == "The specified blob does not exist." && jpgdata.statusText == "The specified blob does not exist." && jpegdata.statusText == "The specified blob does not exist." ) 
+    if (pngdata.statusText == "The specified blob does not exist." && jpgdata.statusText == "The specified blob does not exist." ) 
     {
         success = false; 
         context.log("Does not exist: " + pngdata)
         context.log("Does not exist: " + jpgdata)
-        context.log("Does not exist: " + jpegdata)
     } 
     else if (pngdata.statusText != "The specified blob does not exist.") 
     {
--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -6,17 +6,22 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 module.exports = async function (context, req) {
     var responseMessage = ""
     try{
-        var password = req.headers['codename']; // get the header called "codename" which is how you want to name the file
-        //var the_header_value = req.headers['insert_header_name'];
+        // get the header called "codename" which is how you want to name the file
+        var password = req.headers['codename']; 
+
         // use parse-multipart to parse the body
         var body = req.body;
         var boundary = multipart.getBoundary(req.headers['content-type']);
         var parsedBody = multipart.Parse(body, boundary);
+        
         // determine the file-type here!
         var filetype = parsedBody[0].type;
+        
         if (filetype == "image/png") {
             ext = "png";
         } else if (filetype == "image/jpeg") {
+            ext = "jpeg";
+        } else if (filetype == "image/jpg") {
             ext = "jpg";
         } else {
             username = "invalidimage"
@@ -35,10 +40,12 @@ module.exports = async function (context, req) {
 async function uploadFile(parsedBody, ext, password){ //"ext" for a  file extension
     const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
     const containerName = "images";
-    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
-
-    const blobName = password + "." + ext;    // Create the container
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient(containerName);    
+    // Create the container
+    const blobName = password + "." + ext;   
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName); 
 
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
 
