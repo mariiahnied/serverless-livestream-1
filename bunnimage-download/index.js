da543822b69d276391463f2fa7f0eef7cad9adf8
test <test@Mariias-MacBook-Pro.local>
2021-07-11 14:32:42 -0700

    has to work

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -1,47 +1,54 @@
 const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    let storageName = "bitcampblobstorage";
-    var username = req.headers['username'];
-    var download = ""; 
-    var downloadpng = "https://" + storageName + ".blob.core.windows.net/images/" + username + ".png";
-    var downloadjpg = "https://" + storageName + ".blob.core.windows.net/images/" + username + ".jpeg"; 
+    var username = req.headers['username']; //username is the name of the blob file you wannt to download
+    let blobStorage = "mh1"
+    var download = ""
+    var downloadpng = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".png";
+    var downloadjpg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpg";
+    var downloadjpeg = "https://" + blobStorage + ".blob.core.windows.net/images/" + username + ".jpeg";
 
 
     let pngresp = await fetch(downloadpng, {
         method: 'GET',
-     })
-     let pngdata = await pngresp;
-     
-     let jpgresp = await fetch(downloadjpg, {
-        method: 'GET',
-     })
-     let jpgdata = await jpgresp;
+    })
+    let pngdata = await pngresp;
+    
+    let jpgresp = await fetch(downloadjpg, {
+    method: 'GET',
+    })
+    let jpgdata = await jpgresp;
 
-  
-     if (pngdata.statusText == "The specified blob does not exist." && jpgdata.statusText == "The specified blob does not exist." ) {
+    let jpegresp = await fetch(downloadjpeg, {
+        method: 'GET',
+        })
+    let jpegdata = await jpegresp;
+
+    if (pngdata.statusText == "The specified blob does not exist." && jpgdata.statusText == "The specified blob does not exist." && jpegdata.statusText == "The specified blob does not exist.") {
         success = false;
         context.log("Does not exist: " + pngdata)
         context.log("Does not exist: " + jpgdata)
-     } else if (pngdata.statusText != "The specified blob does not exist.") {
+    } else if (pngdata.statusText != "The specified blob does not exist.") {
         success = true;
         download = downloadpng
         context.log("Does exist: " + pngdata)
-     } else if (jpgdata.statusText != "The specified blob does not exist.") {
+    } else if (jpgdata.statusText != "The specified blob does not exist.") {
         success = true;
         download = downloadjpg
         context.log("Does exist: " + jpgdata)
-     }
-     
-     
-     context.res = {
+    } else if (jpegdata.statusText != "The specified blob does not exist.") {
+        success = true;
+        download = downloadjpeg
+        context.log("Does exist: " + jpegdata)
+    }
+
+
+    context.res = {
         body: {
-                 "downloadUri" : download,
-                 "success": success,
+          "downloadUri" : download,
+          "success": success,
         }
-  };
-  context.log(download);
-  context.done();
-}
+    };
+    context.log(download);
+    context.done();
+};
--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,53 +1,60 @@
-var multipart = require("parse-multipart")
+const fetch = require("node-fetch")
+const multipart = require("parse-multipart")
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-const { BlobServiceClient } = require("@azure/storage-blob");
+const {BlobServiceClient} = require("@azure/storage-blob");
 
+async function uploadFile(parsedBody, ext, password) {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+
+    const containerName = "images";
+
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    // Get a reference to a container
+
+    const blobName = password + "." + ext;
+    // Create the container
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    // Get a block blob client
+
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+    // upload data to blob
+
+    return ("Your blob is saved!");
+};
 
 module.exports = async function (context, req) {
-    var responseMessage = ""
-    try{
-        // get the header called "codename" which is how you want to name the file
-        var password = req.headers['codename']; 
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-        // use parse-multipart to parse the body
-        var body = req.body;
-        var boundary = multipart.getBoundary(req.headers['content-type']);
-        var parsedBody = multipart.Parse(body, boundary);
-        
-        // determine the file-type here!
-        var filetype = parsedBody[0].type;
-        
+    let responseMessage = ""
+
+    try {
+        var password = req.headers['codename'];
+
+        let boundary = multipart.getBoundary(req.headers['content-type']);
+
+        let parsedBody = multipart.Parse(req.body, boundary);
+
+        let filetype = parsedBody[0].type;
+
         if (filetype == "image/png") {
             ext = "png";
         } else if (filetype == "image/jpeg") {
             ext = "jpeg";
         } else if (filetype == "image/jpg") {
-            ext = "jpg";
+            ext = "jpg"
         } else {
             username = "invalidimage"
             ext = "";
         }
         responseMessage = await uploadFile(parsedBody, ext, password);
-    }catch(err){
+    } catch (err) {
         context.log("Undefined body image");
         responseMessage = "Sorry! No image attached."
     }
+
     context.res = {
         body: responseMessage
     };
-}
-
-async function uploadFile(parsedBody, ext, password){ //"ext" for a  file extension
-    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-    const containerName = "images";
-    // Get a reference to a container
-    const containerClient = blobServiceClient.getContainerClient(containerName);    
-    // Create the container
-    const blobName = password + "." + ext;   
-    // Get a block blob client
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName); 
-
-    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
-
-    return "Your blob is saved"
+    context.done();
 }
