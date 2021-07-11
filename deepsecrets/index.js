9c77ab3fe9d961e1c786b4dd8910a16b1839e6d6
test <test@Mariias-MacBook-Pro.local>
2021-07-11 14:58:31 -0700

    uploaded a function that returns the sent test message

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const queryObject = querystring.parse(req.body);
-
+    context.log(queryObject.Body)
     context.res = {
         body: queryObject.Body
     };
