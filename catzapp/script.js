b4e81314b89bb8036a942bb9a0aea7b6cefa02fa
test <test@Mariias-MacBook-Pro.local>
2021-07-27 18:10:06 -0700

    4cats

--- catzapp/script.js
+++ catzapp/script.js
@@ -1,11 +1,59 @@
 async function y1k3s(){
-    let name = document.getElementById("name").value
-    let endpoint = "https://cataas.com/cat/says/" + name
+    let name1 = document.getElementById("name1").value
+    let name2 = document.getElementById("name2").value
+    let name3 = document.getElementById("name3").value
+    let name4 = document.getElementById("name4").value
+    
+    
 
-    if (name != ''){
+
+    let endpoint1 = "https://cataas.com/cat/says/" + name1
+    if (name1 != ''){
       try{
-        document.getElementById("image").src = endpoint
-        document.getElementById("image").alt= "Cat with written name"
+        document.getElementById("image1").src = endpoint1
+        //document.getElementById("image1").alt= "Cat1 with written name"
+      }
+      catch(err){
+            $('#output').text(err)
+        }
+    }
+    else{
+      alert("No name error.")
+    }
+
+    let endpoint2 = "https://cataas.com/cat/says/" + name2
+    if (name2 != ''){
+      try{
+        document.getElementById("image2").src = endpoint2
+        document.getElementById("image2").alt= "Cat2 with written name"
+      }
+      catch(err){
+            $('#output').text(err)
+        }
+    }
+    else{
+      alert("No name error.")
+    }
+
+    let endpoint3 = "https://cataas.com/cat/says/" + name3
+    if (name3 != ''){
+      try{
+        document.getElementById("image3").src = endpoint3
+        document.getElementById("image3").alt= "Cat3 with written name"
+      }
+      catch(err){
+            $('#output').text(err)
+        }
+    }
+    else{
+      alert("No name error.")
+    }
+
+    let endpoint4 = "https://cataas.com/cat/says/" + name4
+    if (name4 != ''){
+      try{
+        document.getElementById("image4").src = endpoint4
+        document.getElementById("image4").alt= "Cat4 with written name"
       }
       catch(err){
             $('#output').text(err)
